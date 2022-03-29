252d6cdadc9acb5bc84c6f43eb0b536f29ae8e40
Mateusz <100692029+Mateusz0231@users.noreply.github.com>
2022-03-29 11:07:05 +0000

    działa prawidłowo

--- Main.java
+++ Main.java
@@ -6,15 +6,16 @@ class Main
         
         int wys = 5;
         int b=3;
-       for(int t=1; t<=b;t++)
-         {
-        for (int i = 1; i<=wys; i++)
-        {
+        for(int t=1; t<=b;t++)
+        for (int i = 2; i<=wys; i++)
+          {
             for(int j=0;j<wys-i;j++)System.out.print(" ");
             for (int j = 0; j<i-1;j++)System.out.print("*");
             System.out.println(" ");
-        }
+                
+          }
+        
   
-    }
+    
     }
 }
