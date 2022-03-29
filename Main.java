eb456eebadc360361d718cf817a8b84913d718f2
Mateusz <100692029+Mateusz0231@users.noreply.github.com>
2022-03-29 10:51:03 +0000

    ni

--- Main.java
+++ Main.java
@@ -3,13 +3,19 @@ class Main
 {
   public static void main(String[] args) 
   {
-        int wys = 4;
+        
+        int wys = 5;
+        int b=3;
+       for(int t=1; t<=b;t++)
+         {
         for (int i = 1; i<=wys; i++)
         {
-            for(int j=0;j<wys-i;j++)System.out.println(" ");
+            for(int j=0;j<wys-i;j++)
+          System.out.println(" ");
             for (int j = 0; j<(i*2)-1;j++)System.out.print("*");
-            
+            System.out.println(" ");
         }
   
+    }
     }
 }
